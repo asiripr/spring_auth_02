e00d86db2ba0ed599146c048e76e073195696f3e
asiripr <asiripramodaya2000@gmail.com>
2024-09-27 21:13:59 +0530

    for learn authentication and autherization

---
+++ src/main/java/lk/asiripr/auth_02/Auth02Application.java
@@ -0,0 +1,13 @@
+package lk.asiripr.auth_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth02Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/asiripr/auth_02/Auth02ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.asiripr.auth_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
