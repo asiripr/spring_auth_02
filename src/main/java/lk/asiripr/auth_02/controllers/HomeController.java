80ce0e77b4507f8e4eefa4e02f208c16418e3210
asiripr <asiripramodaya2000@gmail.com>
2024-09-27 21:25:14 +0530

    created the structure

---
+++ src/main/java/lk/asiripr/auth_02/controllers/HomeController.java
@@ -0,0 +1,8 @@
+package lk.asiripr.auth_02.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+}
---
+++ src/main/java/lk/asiripr/auth_02/services/CustomUserDetailsServices.java
@@ -0,0 +1,8 @@
+package lk.asiripr.auth_02.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsServices {
+
+}
